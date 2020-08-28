c4c92867c3e10e1a855edf1c7dc53b4fd9bddf00
JAB-GitHub <58780714+JAB-GitHub@users.noreply.github.com>
2020-08-28 11:33:27 -0300

    Update sign.c
    can cont time and clock of XMSS_SIGN.

--- ui/sign.c
+++ ui/sign.c
@@ -86,8 +86,59 @@ int main(int argc, char **argv) {
     fread(sk, 1, XMSS_OID_LEN + params.sk_bytes, keypair_file);
     fread(m, 1, mlen, m_file);
 
+ /* 
+    * STAR timer
+    */
+    
+	//Abertura do arquivo que vai receber o tempo de criação das chaves
+	FILE *file_time = fopen("Time to mult sign.txt", "a"); 
+	
+	//Verificação se o arquivo abriu
+	if (file_time == NULL){
+		printf("Erro para abrir o arquivo!\n");
+		return -1;
+	}
+	
+	//Abertura do arquivo que vai receber o clock para a criação das chaves
+	FILE *file_time1 = fopen("Clock to mult sign.txt", "a"); 
+	
+	//Verificação se o arquivo abriu
+	if (file_time1 == NULL){
+		printf("Erro para abrir o arquivo!\n");
+		return -1;
+	}	
+	
+	long long int clock;
+	double time_spent = 0.0; //Varaiável que receberá o valor do tempo de execução
+	clock_t begin = clock(); //Comça a contagem de operações de máquina
+    /*
+    * Will cont time to execute XMSS_KEYPAIR
+    */    
+    
     XMSS_SIGN(sk, sm, &smlen, m, mlen);
+    /*
+    * STOP timer
+    */
+    fclose(stdout);
 
+	clock_t end = clock(); //Para a contagem de operações
+	
+	time_spent +=(double)(end - begin) / CLOCKS_PER_SEC; //Divide a quantidades de operações pela frequência do clock para obter o tempo em segundo
+	
+	clock = end - begin;
+	
+	// Regista no arquivo o tempo para criação do arquivo
+	fprintf(file_time,"%lf;", time_spent);
+	
+	// Regista no arquivo o clock para criação do arquivo
+	fprintf(file_time1,"%lld;", clock);
+	
+	//Fecha os arquivos
+	fclose(file_time);
+	fclose(file_time1);
+    /*
+    * Save time and clock in .txt
+    */
     fseek(keypair_file, -((long int)params.sk_bytes), SEEK_CUR);
     fwrite(sk + XMSS_OID_LEN, 1, params.sk_bytes, keypair_file);
     fwrite(sm, 1, smlen, stdout);
