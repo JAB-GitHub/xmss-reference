a4fba539df2dff1dfdde36580bb98ebcd2d00804
JAB-GitHub <58780714+JAB-GitHub@users.noreply.github.com>
2020-08-28 11:29:49 -0300

    Update keypair.c
    Now can cont time to execute the function to create pair key.

--- ui/keypair.c
+++ ui/keypair.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include<time.h>
 
 #include "../params.h"
 #include "../xmss.h"
@@ -36,9 +37,62 @@ int main(int argc, char **argv)
 
     unsigned char pk[XMSS_OID_LEN + params.pk_bytes];
     unsigned char sk[XMSS_OID_LEN + params.sk_bytes];
-
+    
+    /* 
+    * STAR timer
+    */
+    
+	//Abertura do arquivo que vai receber o tempo de criação das chaves
+	FILE *file_time = fopen("Time to mult keypair generacion.txt", "a"); 
+	
+	//Verificação se o arquivo abriu
+	if (file_time == NULL){
+		printf("Erro para abrir o arquivo!\n");
+		return -1;
+	}
+	
+	//Abertura do arquivo que vai receber o clock para a criação das chaves
+	FILE *file_time1 = fopen("Clock to mult keypair generacion.txt", "a"); 
+	
+	//Verificação se o arquivo abriu
+	if (file_time1 == NULL){
+		printf("Erro para abrir o arquivo!\n");
+		return -1;
+	}	
+	
+	long long int clock;
+	double time_spent = 0.0; //Varaiável que receberá o valor do tempo de execução
+	clock_t begin = clock(); //Comça a contagem de operações de máquina
+    /*
+    * Will cont time to execute XMSS_KEYPAIR
+    */
+    
     XMSS_KEYPAIR(pk, sk, oid);
 
+    /*
+    * STOP timer
+    */
+    fclose(stdout);
+
+	clock_t end = clock(); //Para a contagem de operações
+	
+	time_spent +=(double)(end - begin) / CLOCKS_PER_SEC; //Divide a quantidades de operações pela frequência do clock para obter o tempo em segundo
+	
+	clock = end - begin;
+	
+	// Regista no arquivo o tempo para criação do arquivo
+	fprintf(file_time,"%lf;", time_spent);
+	
+	// Regista no arquivo o clock para criação do arquivo
+	fprintf(file_time1,"%lld;", clock);
+	
+	//Fecha os arquivos
+	fclose(file_time);
+	fclose(file_time1);
+    /*
+    * Save time and clo in .txt
+    */
+    
     fwrite(pk, 1, XMSS_OID_LEN + params.pk_bytes, stdout);
     fwrite(sk, 1, XMSS_OID_LEN + params.sk_bytes, stdout);
 
